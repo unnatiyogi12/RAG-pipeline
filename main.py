ea90caadfec30d940bd8e553b0754b3e431d07b5
unnatiyogi12 <unnatiyogi170@gmail.com>
2026-04-14 01:31:48 +0530

    test spliiting done

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 data = PyPDFLoader("data/pdf/DSML.pdf")
 docs = data.load()
 
+splitter = (
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+
 template = ChatPromptTemplate.from_messages(
     [("system", "you are a AI that summarizes the text"),
      ("human", "{data}")]
 )
+
+chunks = splitter.split_documents(docs)
+
 model = ChatMistralAI(model = "mistral-small-2506" \
 "")
 
-prompt = template.format_messages(data= docs[0].page_content)
+prompt = template.format_messages(data= docs)
+
 result = model.invoke(prompt)
 print(result.content)
